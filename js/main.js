31d4e16bb5764881f1a4c0759ebd4ebac91ae40b
Mayra Alvarez <mayra.alvarez.velasquez@gmail.com>
2016-10-10 13:08:24 -0500

    agregando archivos

---
+++ js/main.js
@@ -0,0 +1,53 @@
+var avatar = [{
+    name: "Laboratoria Peru",
+    foto: "logocodecademy.png",
+    mensaje: " "
+}, {
+    name: "Raymi Saldomando",
+    foto: "raymi.jpg",
+    mensaje: " "
+}, {
+    name: "Mariana Costa",
+    foto: "mariana.jpg",
+    mensaje: " "
+}, {
+    name: "Ana Maria Martinez Franklin",
+    foto: "anamaria.jpg",
+    mensaje: " "
+}, {
+    name: "Rodulfo Prieto",
+    foto: "rodulfo.jpg",
+    mensaje: " "
+}, {
+    name: "Andrea Lamas",
+    foto: "andrea.jpg",
+    mensaje: " "
+}, {
+    name: "Maria Paula Rivarola",
+    foto: "mariapaula.jpg",
+    mensaje: " "
+}, {
+    name: "Kathy Sanchez",
+    foto: "katy.jpg",
+    mensaje: " "
+}, {
+    name: "Aldo Alfaro",
+    foto: "aldo.jpg",
+    mensaje: " "
+}];
+
+window.addEventListener("load", function(){
+
+  var inputTexto = document.getElementsByClassName("w-search-contacts"); 
+  var avatar = document.getElementsByClassName("avatar"); 
+  var img = document.getElementsByClassName("wh-44"); 
+  var nombreCompleto = document.getElementsByClassName("w-contact-name"); 
+  var mensaje = document.getElementById("mensaje"); 
+  var hora = document.getElementById("hora");
+  var conversacion = document.getElementsByClassName("conversacion"); 
+
+  function inputTexto (){
+
+  }
+  
+});
